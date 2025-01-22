dc94cffd97ced9ec3d4b3e6bfa2c622051412aef
Lucky <VIVAANBRADOOEDU@GMAIL.COM>
2025-01-22 10:42:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -90,4 +90,5 @@ def delete_quiz(quiz_id):
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
