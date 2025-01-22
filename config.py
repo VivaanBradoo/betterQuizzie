ddff83e51d2e05b64462e32ad83b47381a3daf9d
Vivaan Bradoo <vivaanbradooedu@gmail.com>
2025-01-22 09:31:53 +0530

    changed database url

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://database_owner:npg_eHnpN6W3qXGZ@ep-dry-voice-a97peo6t.gwc.azure.neon.tech/database?sslmode=require'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
