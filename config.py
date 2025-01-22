f34a819496f7ffa402d603d15fda0735e5987f58
Lucky <VIVAANBRADOOEDU@GMAIL.COM>
2025-01-22 11:55:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = 'your_secret_key'
+    SECRET_KEY = os.environ.get("MAIN_KEY")
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
